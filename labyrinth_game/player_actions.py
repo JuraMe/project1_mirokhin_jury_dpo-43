27dd86ee4aaca3ff2bf22a8f6fd75a98f64d19cd
Jura.me <m@jura.me>
2025-11-11 18:31:23 +0300

    2.3.2. + Функция взятия предмета

--- labyrinth_game/player_actions.py
+++ labyrinth_game/player_actions.py
@@ -2,6 +2,7 @@
 from typing import Dict
 from labyrinth_game.constants import ROOMS
 
+# Функция отображения инвентаря
 def show_inventory(game_state: Dict):
     inv = game_state.get("player_inventory", [])
     if not inv:
@@ -11,6 +12,7 @@ def show_inventory(game_state: Dict):
     for item in inv:
         print(f"  - {item}")
 
+#Ввод пользователя
 def get_input(prompt: str = "> ") -> str:
     try:
         return input(prompt).strip()
@@ -18,7 +20,7 @@ def get_input(prompt: str = "> ") -> str:
         print("\nВыход из игры.")
         return "quit"
 
-
+# Функция перемещения
 def move_player(game_state, direction: str):
     current = game_state["current_room"]
     room = ROOMS.get(current, {})
@@ -33,3 +35,17 @@ def move_player(game_state, direction: str):
         describe_current_room(game_state)
     else:
         print("Нельзя пойти в этом направлении.")
+
+# Функция взятия предмета
+def take_item(game_state, item_name: str):
+    item_name = item_name.strip()
+    current = game_state["current_room"]
+    room = ROOMS.get(current, {})
+    items = room.get("items", [])
+
+    if item_name in items:
+        game_state["player_inventory"].append(item_name)
+        items.remove(item_name)
+        print(f"Вы подняли: {item_name}")
+    else:
+        print("Такого предмета здесь нет.")
